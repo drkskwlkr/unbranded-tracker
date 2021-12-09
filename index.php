11e8d4e79c90363477de172c95115de7c8828e85
TotalBuzzKit <drkskwlkr@users.noreply.github.com>
2021-12-09 17:36:57 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php require_once( $_SERVER['DOCUMENT_ROOT'] . '/config.inc.php') ; ?>
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo $SITE_TITLE ;?></title>
+  <link rel="canonical" href="<?php echo $SITE_URL ; ?>" />
+  <link rel="stylesheet" href="./style.css" />
+</head>
+
+<body>
+
+	<!-- Main Area Begin -->
+	<section id="main">
+		<h1 class="maintitle">Информация за <span class="optional"> движението на </span>Вашата пратка</h1>		
+
+		<?php require_once( $sitebasepath=$_SERVER['DOCUMENT_ROOT'] . '/track.php') ; ?>
+
+	</section>
+	<!-- Main Area End -->
+
+	<section id="additional">
+
+		<p>&nbsp;</p>
+
+	</section>
+
+	<!-- Pixel & Tracking Code Area Begin -->
+  
+  <!-- Pixel & Tracking Code Area End -->
+</body>
+</html>
