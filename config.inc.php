c28b09427ac0e2aeb9cec5ed4c65b5ea44381fe3
TotalBuzzKit <drkskwlkr@users.noreply.github.com>
2021-12-09 18:12:07 +0200

    Create config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,28 @@
+<?php
+# Some variables we will need
+
+# Administrative
+define ('SITE_TITLE', "") ; // Insert tracking website name inside dobule quotes, e.g. define ('SITE_TITLE', "Магазин Example.com: движение на доставките") ;
+define ('SITE_URL',   "") ; // Insert tracking website address including protocol inside double quotes, e.g. define ('SITE_URL', "https://tracking.example.com") ;
+
+# ------- SPEEDY -------
+# Speedy User details
+define ('SPEEDY_USER', "") ;        // Insert username Speedy API username inside quotes, e.g. define ('SPEEDY_USER', "912345") ;
+define ('SPEEDY_PASS', "") ;        // Insert password for Speedy API inside quotes, e.g. define ('SPEEDY_PASS', "12345678901") ;
+define ('SPEEDY_LANG', "bg") ;      // Do not change this unless instructed by the developer
+
+# Speedy API details
+define ('SPEEDY_API_BASE', "https://api.speedy.bg/v1/") ; // Do not change this unless instructed by the developer
+define ('SPEEDY_API_CMD_TRACK', "track") ;                // Do not change this unless instructed by the developer
+
+# Speedy API command syntax
+$callSpeedyTrack = SPEEDY_API_BASE . SPEEDY_API_CMD_TRACK . '?userName=' . SPEEDY_USER . '&password=' . SPEEDY_PASS . '&language=' . SPEEDY_LANG . '&parcels=' ;
+
+
+# ------- ECONT ---------
+# Econt API details
+define ('ECONT_API_BASE', "https://ee.econt.com/services/") ;                            // Do not change this unless instructed by the developer
+define ('ECONT_API_CMD_TRACK', "Shipments/ShipmentService.getShipmentStatuses.json") ;   // Do not change this unless instructed by the developer
+
+# Econt API syntax
+$callEcontTrack = ECONT_API_BASE . ECONT_API_CMD_TRACK ;
