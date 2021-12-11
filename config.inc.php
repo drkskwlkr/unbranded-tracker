d16de515dfcfc4ecabfa407e339ab6a7a9d15860
TotalBuzzKit <drkskwlkr@users.noreply.github.com>
2021-12-11 18:36:00 +0200

    Update config.inc.php
    Added default language (Bulgarian) for tracking queries

--- config.inc.php
+++ config.inc.php
@@ -1,30 +1,24 @@
 <?php
-# Administrative
-define ('SITE_TITLE',        "") ; // Insert tracking website name inside dobule quotes, e.g. define ('SITE_TITLE', "Магазин Example.com: движение на доставките") ;
-define ('SITE_URL',          "") ; // Insert tracking website address including protocol inside double quotes, e.g. define ('SITE_URL', "https://tracking.example.com") ;
-define ('SITE_CONTACT_URL',  "") ; // Insert contact website address including protocol, e.g. define ('SITE_CONTACT_URL', "https://www.example.com/contact") ;
+# General
+define ('SITE_TITLE',					"") ; // Insert tracking website name inside dobule quotes, e.g. define ('SITE_TITLE', "Магазин Example.com: движение на доставките") ;
+define ('SITE_URL',						"") ; // Insert tracking website address including protocol inside double quotes, e.g. define ('SITE_URL', "https://tracking.example.com") ;
+define ('SITE_CONTACT_URL',		"") ; // Insert contact website address including protocol, e.g. define ('SITE_CONTACT_URL', "https://www.example.com/contact") ;
+define ('LANGUAGE_DEFAULT',		"bg") ; // Determine default language for return queries. Specify either bg or en (small caps)
 
 # ------- SPEEDY -------
 # Speedy User details
 define ('SPEEDY_USER', "") ;        // Insert username Speedy API username inside quotes, e.g. define ('SPEEDY_USER', "912345") ;
 define ('SPEEDY_PASS', "") ;        // Insert password for Speedy API inside quotes, e.g. define ('SPEEDY_PASS', "12345678901") ;
-define ('SPEEDY_LANG', "bg") ;      // Do not change this unless instructed by the developer
 
 # Speedy API details
 define ('SPEEDY_API_BASE', "https://api.speedy.bg/v1/") ; // Do not change this unless instructed by the developer
 define ('SPEEDY_API_CMD_TRACK', "track") ;                // Do not change this unless instructed by the developer
 
-# Speedy API command syntax
-$callSpeedyTrack = SPEEDY_API_BASE . SPEEDY_API_CMD_TRACK . '?userName=' . SPEEDY_USER . '&password=' . SPEEDY_PASS . '&language=' . SPEEDY_LANG . '&parcels=' ;
-
 
 # ------- ECONT ---------
 # Econt API details
-define ('ECONT_API_BASE', "https://ee.econt.com/services/") ;                            // Do not change this unless instructed by the developer
-define ('ECONT_API_CMD_TRACK', "Shipments/ShipmentService.getShipmentStatuses.json") ;   // Do not change this unless instructed by the developer
-
-# Econt API command syntax
-$callEcontTrack = ECONT_API_BASE .	ECONT_API_CMD_TRACK ;
+define ('ECONT_API_BASE', 			"https://ee.econt.com/services/") ;                            // Do not change this unless instructed by the developer
+define ('ECONT_API_CMD_TRACK',	"Shipments/ShipmentService.getShipmentStatuses.json") ;   // Do not change this unless instructed by the developer
 
 
 # ------- A1 POST -------
