3655cee2361f4c96911bdac039cab1787a77f6e9
TotalBuzzKit <drkskwlkr@users.noreply.github.com>
2021-12-23 02:05:41 +0200

    Update track.php
    Introduced a bug. Fixed now.

--- track.php
+++ track.php
@@ -337,12 +337,12 @@ function printCVC($parcel_id, $language_id) {
 		echo " &rarr; " ;
 		echo '<span class="monoblocked">' . $operations[$i]['display'] . '</span> <span class="monoblocked">' . $operations[$i]['station'] . '</span>' ;
 		echo '</div>' ;
-	}
-
-	/* If package has been delivered, show the delivery notice and ask for a review */
-	if ("Доставена - без възражения" === $operations[$i]['display']) { // Package has been delivered
-		echo '<h3 class="h3delivered">Пратката е доставена</h3>' . "\n" ;
-		feedbackRequestGoogle() ;
+		
+		/* If package has been delivered, show the delivery notice and ask for a review */
+		if ("Доставена - без възражения" === $operations[$i]['display']) { // Package has been delivered
+			echo '<h3 class="h3delivered">Пратката е доставена</h3>' . "\n" ;
+			feedbackRequestGoogle() ;
 		break ;
+		}
 	}
 }
